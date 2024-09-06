d1731ccb866411b24ed51a9493229ad9ff1e184f
andrewhara <137232206+andrewhara@users.noreply.github.com>
2024-09-06 09:42:20 -0700

    Add files via upload

---
+++ Initials.py
@@ -0,0 +1,24 @@
+#File: Initials.py
+#Student: Andrew Hara
+#
+#Date: September 4, 2024
+#Description of Program: Prints my initials (A.K.H) in large letters of width 12 and height 10 characters
+#My Github:
+
+def initials():
+    print("This is a program that prints out my initials (A.K.H.)")
+    print()
+    print("      AA            KK        KK      HH        HH      ")
+    print("     AA AA          KK      KK        HH        HH      ")
+    print("    AA   AA         KK    KK          HH        HH      ")
+    print("   AA     AA        KK   KK           HH        HH      ")
+    print("  AA       AA       KK  KK            HHHHHHHHHHHH      ")
+    print("  AAAAAAAAAAAA      KKKK              HHHHHHHHHHHH      ")
+    print("  AAAAAAAAAAAA      KKKK              HH        HH      ")
+    print("  AA        AA      KK  KK            HH        HH      ")
+    print("  AA        AA      KK    KK          HH        HH      ")
+    print("  AA        AA  ..  KK      KK    ..  HH        HH  ..  ")
+    print("  AA        AA  ..  KK        KK  ..  HH        HH  ..  ")
+    print()
+
+initials()
