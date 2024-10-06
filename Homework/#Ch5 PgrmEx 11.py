f5cd091c351be90943ca94f652ba190114f94c90
andrewhara <137232206+andrewhara@users.noreply.github.com>
2024-10-05 23:34:14 -0700

    Add files via upload

---
+++ Homework/#Ch5 PgrmEx 11.py
@@ -0,0 +1,17 @@
+#Ch5 PgrmEx 11
+import random
+
+def math_test():
+    
+    for i in range(1, 6):
+        num1 = random.randint (1, 10)
+        num2 = random.randint (1, 10)
+        print("Question", i)
+        print()
+        
+        print(num1, "+", num2, "=", "___")
+        print()
+        
+    
+math_test()
+    
